03e782584a82cac00e2dc830743ac2e5821c01ff
Juan Calderon Ardila <airdevice@Juan-Calderon-Ardila.local>
2023-07-31 22:14:37 +0200

    first push

---
+++ client.c
@@ -0,0 +1,48 @@
+#include <signal.h>
+#include "ft_printf/ft_printf.h"
+
+void send_char(pid_t pid, char c)
+{
+    int i;
+
+    i = 7;
+    while (i >= 0)
+    {
+        usleep(100); // pause for a moment
+        if ((c >> i) & 1)
+        {
+            kill(pid, SIGUSR2);
+        }
+        else
+        {
+            kill(pid, SIGUSR1);
+        }
+        i--;
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    int server_pid;
+    char* message;
+
+    int  i;
+    if (argc != 3)
+    {
+        ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
+        return (1);
+    }
+
+    // Convert server_pid from string to integer
+    server_pid = ft_atoi(argv[1]);
+    message = argv[2];
+
+    // Then send the actual message
+    i = 0;
+    while (i < ft_strlen(message))
+    {
+        send_char(server_pid, message[i]);
+        i++;
+    }
+    return (0);
+}
---
+++ server.c
@@ -0,0 +1,50 @@
+#include <signal.h>
+#include "ft_printf/ft_printf.h"
+
+struct s_server
+{
+    int bit_count;
+    int character;
+} g_server = {0, 0};
+
+void process_character(void)
+{
+    ft_printf("%c", g_server.character);
+    fflush(stdout);
+    g_server.character = 0;
+    g_server.bit_count = 0;
+}
+
+void handle_signal(int signal)
+{
+    g_server.character <<= 1;
+    if (signal == SIGUSR2)
+    {
+        g_server.character += 1;
+    }
+    g_server.bit_count++;
+
+    if (g_server.bit_count == 8)
+    {
+        process_character();
+    }
+}
+
+int main(void)
+{
+    struct sigaction sa;
+
+    g_server.bit_count = 0;
+    g_server.character = 0;
+    sa.sa_handler = &handle_signal;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+    ft_printf("Server PID: %d\n", getpid());
+    while (1)
+    {
+        pause();
+    }
+    return (0);
+}
